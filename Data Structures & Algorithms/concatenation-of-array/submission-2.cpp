22628c72c98cef4448f5178b0f792e7b9a97e4fc
Neil Luong <132160648+NeilLuong@users.noreply.github.com>
2026-09-23 22:40:08 -0400

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+
+        auto ans = nums;
+        ans.reserve(nums.size()*2);
+
+        ans.insert(ans.end(), nums.begin(), nums.end());
+
+        return ans;
+    }
+};
